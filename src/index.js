62a9b9d6ec956b51604771dc2160357e2a799979
AllaPavlovska <pavlovskaa87@gmail.com>
2023-11-04 19:03:06 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,65 +1,92 @@
+
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
+
 const refs = {
   breedSelect: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
   catInfo: document.querySelector('.cat-info'),
 };
+
 new SlimSelect('.breed-select');
-refs.breedSelect.style.width = '200px';
-refs.breedSelect.style.height = '30px';
-refs.breedSelect.style.fontSize = '15px';
+
+const setElementStyle = (element, styles) => {
+  for (const [property, value] of Object.entries(styles)) {
+    element.style[property] = value;
+  }
+};
+
+setElementStyle(refs.breedSelect, {
+  width: '200px',
+  height: '30px',
+  fontSize: '15px',
+});
+
 function hideLoader() {
-  refs.loader.style.display = 'none';
+  setElementStyle(refs.loader, { display: 'none' });
   refs.breedSelect.disabled = false;
-}
-function showloader() {
-  refs.loader.style.display = 'block';
-  refs.breedSelect.disabled = false;
-  refs.catInfo.style.display = 'none';
-  refs.error.style.display = 'none';
 }
 
-function showError() {
-  Notiflix.Notify.Failure('Oops! Something went wrong. Try reloading the page.');
+function showLoader() {
+  setElementStyle(refs.loader, { display: 'block' });
+  refs.breedSelect.disabled = false;
+  setElementStyle(refs.catInfo, { display: 'none' });
+  setElementStyle(refs.error, { display: 'none' });
+}
+
+function showError(message) {
+  Notiflix.Notify.Failure(message || 'Oops! Something went wrong. Try reloading the page.');
   hideLoader();
 }
-fetchBreeds()
-  .then(breeds => {
+
+(async function () {
+  try {
+    showLoader();
+    const breeds = await fetchBreeds();
     hideLoader();
+
     breeds.forEach(breed => {
       const option = document.createElement('option');
       option.value = breed.id;
       option.textContent = breed.name;
       refs.breedSelect.appendChild(option);
     });
-  })
-  .catch(() => {
+
+    refs.breedSelect.addEventListener('change', async () => {
+      showLoader();
+      const selectedBreedId = refs.breedSelect.value;
+
+      if (selectedBreedId) {
+        try {
+          const catData = await fetchCatByBreed(selectedBreedId);
+
+          if (catData && catData.length > 0) {
+            const cat = catData[0];
+            refs.catInfo.innerHTML = `
+              <img src="${cat.url}" alt="${cat.breeds[0].name}">
+              <h2>${cat.breeds[0].name}</h2>
+              <p><strong>Description:</strong> ${cat.breeds[0].description}</p>
+              <p><strong>Temperament:</strong> ${cat.breeds[0].temperament}</p>
+            `;
+            setElementStyle(refs.catInfo, { width: '800px', display: 'block' });
+          } else {
+            showError('No cat data found for the selected breed.');
+          }
+
+          hideLoader();
+        } catch (error) {
+          hideLoader();
+          showError('Error fetching cat information.');
+          console.error(error);
+        }
+      }
+    });
+  } catch (error) {
     hideLoader();
-    showError();
-  });
-refs.breedSelect.addEventListener('change', () => {
-  showloader();
-  const selectedBreedId = refs.breedSelect.value;
-  if (selectedBreedId) {
-    fetchCatByBreed(selectedBreedId)
-      .then(catData => {
-        hideLoader();
-        const cat = catData[0];
-        refs.catInfo.innerHTML = `
-                 <img src="${cat.url}" alt="${cat.breeds[0].name}">
-          <h2>${cat.breeds[0].name}</h2>
-          <p><strong>Description:</strong> ${cat.breeds[0].description}</p>
-          <p><strong>Temperament:</strong> ${cat.breeds[0].temperament}</p>
-                `;
-        refs.catInfo.style.width = '800px';
-        refs.catInfo.style.display = 'block';
-      })
-      .catch(() => {
-        hideLoader();
-        showError();
-      });
+    showError('Error fetching breed information.');
+    console.error(error);
   }
-});
+})();
+
